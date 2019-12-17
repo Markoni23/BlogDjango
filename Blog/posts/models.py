f71431aff0f8d236aac303616679919365533b15
Mark Zhigulin <forsteam.markoni@mail.ru>
2019-12-18 00:25:29 +0300

    making posts and start login

--- Blog/posts/models.py
+++ Blog/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, 
+                                on_delete=models.CASCADE)
--- Blog/posts/views.py
+++ Blog/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class PostsListView(LoginRequiredMixin, ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    template_name = 'posts/posts.html'
