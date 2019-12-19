892dd29bac5949446c290d02b645643b0ed4870a
Mark Zhigulin <forsteam.markoni@mail.ru>
2019-12-19 14:15:59 +0300

    lil fix2

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -130,6 +130,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'forsteam.markoni@gmail.com'
-EMAIL_HOST_PASSWORD = 'xdvfhrxd'
+EMAIL_HOST_PASSWORD = '*********'
 DEFAULT_FROM_EMAIL = 'forsteam.markoni@gmail.com'
 DEFAULT_TO_EMAIL = 'forsteam.markoni@mail.ru'
